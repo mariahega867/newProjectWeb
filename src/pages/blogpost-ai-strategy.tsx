26b1c678aa18b7272db35b0f7841c96208227905
mariahega867 <mariahernandez867@gmail.com>
2025-05-31 09:29:49 -0700

    fixing bugs

--- src/pages/blogpost-ai-strategy.tsx
+++ src/pages/blogpost-ai-strategy.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
+import { blogPostContent } from './blogpost-content';
 
 export function BlogPostAIStrategy() {
   return (
     <div className="min-h-screen bg-black">
-      <div dangerouslySetInnerHTML={{ __html: `
-        <div style="color: #ffffff; font-family: Arial, sans-serif; line-height: 1.6;">
-          <style>
-            p, .wp-block-paragraph, ul.wp-block-list, li {
-              color: #ffffff !important;
-              font-size: 20px !important;
-            }
-            a {
-              color: #00c2ff !important;
-            }
-          </style>
-          <!-- Content from blogpost.html -->
-          ${require('./blogpost.html')}
-        </div>
-      `}} />
+      <div dangerouslySetInnerHTML={{ __html: blogPostContent }} />
     </div>
   );
 } 
