cc004194c51c4daf2a480712c34869289ad62665
mariahega867 <mariahernandez867@gmail.com>
2025-06-13 23:43:02 -0700

    clean up

--- src/app.tsx
+++ src/app.tsx
@@ -6,11 +6,9 @@ import { Home } from './pages/home';
 import { News } from './pages/news';
 import { Careers } from './pages/careers';
 import { Blog } from './pages/blog';
-import { BlogPost } from './pages/blogpost';
+import { blogPostContent } from './pages/ai-strategy';
 import { BlogPostAIStrategy } from './pages/blogpost-ai-strategy';
-import { BlogPostWildRide } from './pages/blogpostwildride';
-import { BusinessAutomationBlogPost } from './pages/blogpost-business-automation';
-import { MachineLearningBlogPost } from './pages/ machine-learning-consulting-services-guides';
+
 
 function App() {
   return (
@@ -23,15 +21,8 @@ function App() {
             <Route path="/news" element={<News />} />
             <Route path="/careers" element={<Careers />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/ai-unleashed" element={<BlogPost />} />
-            <Route path="/blog/ai-wild-ride" element={<BlogPostWildRide />} />
             <Route path="/blog/ai-strategy" element={<BlogPostAIStrategy />} />
-            <Route path="/blog/business-automation" element={<BusinessAutomationBlogPost />} />
-            <Route path="/blog/implementing-ai-solutions" element={<BlogPost />} />
-            <Route path="/blog/machine-learning-consulting-services-guide" element={<MachineLearningBlogPost />} />
-            <Route path="/blog/machine-learning-guide" element={<BlogPost />} />
-            <Route path="/blog/ai-trends-2024" element={<BlogPost />} />
-            <Route path="/blog/ai-security-guide" element={<BlogPost />} />
+            <Route path="/blog/implementing-ai-solutions" element={<BlogPostAIStrategy />} />
           </Routes>
         </ErrorBoundary>
       </div>
--- src/pages/blogpost-ai-strategy.tsx
+++ src/pages/blogpost-ai-strategy.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Calendar, Clock, Tag, ArrowLeft } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import { blogPostContent } from './blogpost-content';
+import { blogPostContent } from './ai-strategy';
 
 export function BlogPostAIStrategy() {
   return (
